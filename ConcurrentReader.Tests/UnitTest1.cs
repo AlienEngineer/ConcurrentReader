b402ef0687c48c36ee538b8e55677355fbc867cf
AlienEngineer <Alien.Software.Engineer@gmail.com>
2013-02-19 11:14:28 +0000

    Added the tests Project

---
+++ ConcurrentReader.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace ConcurrentReader.Tests
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
